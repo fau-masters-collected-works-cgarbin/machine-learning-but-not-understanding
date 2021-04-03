77da1ccb0e0a56b26f21eb87c34926e4c3cb2b99
Christian Garbin <garbin.christian@gmail.com>
2021-04-03 15:13:13 -0400

    Create triangle dataset

--- generate_images.py
+++ generate_images.py
@@ -22,6 +22,13 @@ BACKGROUND_COLOR = 255
 SQUARE_UPRIGHT = 'square-upright'
 SQUARE_UPRIGHT_FILE = '{}{}{}-{}'.format(IMAGE_DIRECTORY, os.path.sep, SQUARE_UPRIGHT,
                                          '{:0>3d}-{:0>3d}-{:0>3d}-{:0>3d}--{:0>3d}-{:0>3d}-{:0>3d}-{:0>3d}.bmp')
+SQUARE_SIDE = 10
+
+TRIANGLE_UPRIGHT = 'triangle-upright'
+TRIANGLE_UPRIGHT_FILE = '{}{}{}-{}'.format(IMAGE_DIRECTORY, os.path.sep, TRIANGLE_UPRIGHT,
+                                           '{:0>3d}-{:0>3d}-{:0>3d}-{:0>3d}-{:0>3d}-{:0>3d}.bmp')
+TRIANGLE_HEIGHT = 20
+TRIANGLE_BASE = 21
 
 
 def _prepare():
@@ -53,6 +60,21 @@ def _create_square(base_name: str, top_left: Tuple[int, ...], top_right: Tuple[i
     im.save(base_name.format(*top_left, *top_right, *bottom_left, *bottom_right))
 
 
+def _create_triangle(base_name: str, vertex1: Tuple[int, ...], vertex2: Tuple[int, ...], vertex3: Tuple[int, ...]):
+    """Create a triangle picture and save it.
+
+    All coordinates are in the Pillow system: the top-left corner of the canvas is (0,0).
+
+    Args:
+        base_name (str): Base name for the file where the picture will be saved
+        vertex1..3 (Tuple[int, ...]): [x, y] coordinate of each vertex
+    """
+    im = Image.new(mode='L', size=(CANVAS_SIZE, CANVAS_SIZE), color=BACKGROUND_COLOR)
+    draw = ImageDraw.Draw(im)
+    draw.polygon([vertex1, vertex2, vertex3], outline=0)
+    im.save(base_name.format(*vertex1, *vertex2, *vertex3))
+
+
 def _display_grayscale_image_hex(file: str):
     data = image.imread(file)
     print('Data type: {}, shape: {}'.format(data.dtype, data.shape))
@@ -62,27 +84,45 @@ def _display_grayscale_image_hex(file: str):
 
 
 def create_upright_square_dataset():
-    """Create a dataset of upright square iamges."""
-    SIDE = 10
-    for y in range(2, 50, 2):
-        for x in range(2, 50, 2):
-            right_x = x+SIDE-1
-            bottom_y = y+SIDE-1
+    """Create a dataset of upright square iamges.
+
+    The range in the loops are set to have about the same number of squares and triangles.
+    """
+    for y in range(2, 48, 2):
+        for x in range(2, 45, 3):
+            right_x = x+SQUARE_SIDE-1
+            bottom_y = y+SQUARE_SIDE-1
             _create_square(SQUARE_UPRIGHT_FILE, (x, y), (right_x, y), (x, bottom_y), (right_x, bottom_y))
 
 
-def test():
+def create_upright_triangle_dataset():
+    """Create a dataset of upright triangle iamges."""
+    half_base = TRIANGLE_BASE // 2
+    for y in range(TRIANGLE_HEIGHT+2, 50, 2):
+        for x in range(2, 50, 2):
+            _create_triangle(TRIANGLE_UPRIGHT_FILE, (x, y), (x+half_base, y-TRIANGLE_HEIGHT+1),
+                             (x+TRIANGLE_BASE-1, y))
+
+
+def test(square: bool):
     """Test code to check that grayscale images are properly generated.
 
     It creates one image, then displays the pixel values (in hexadecimal), so we can visually
     inspect that the image type and pixels values are correct.
     """
-    coordinates = ((2, 2), (29, 2), (2, 29), (29, 29))
-    _create_square(SQUARE_UPRIGHT_FILE, *coordinates)
-    _display_grayscale_image_hex(SQUARE_UPRIGHT_FILE.format(*[c for tupl in coordinates for c in tupl]))
+    if square:
+        coordinates = ((2, 2), (29, 2), (2, 29), (29, 29))
+        _create_square(SQUARE_UPRIGHT_FILE, *coordinates)
+        _display_grayscale_image_hex(SQUARE_UPRIGHT_FILE.format(*[c for tupl in coordinates for c in tupl]))
+    else:
+        coordinates = ((2, 11), (5, 2), (9, 11))
+        _create_triangle(TRIANGLE_UPRIGHT_FILE, *coordinates)
+        _display_grayscale_image_hex(TRIANGLE_UPRIGHT_FILE.format(*[c for tupl in coordinates for c in tupl]))
 
 
 if __name__ == "__main__":
     _prepare()
-    # test()
+    # test(true)
+    # test(false)
     create_upright_square_dataset()
+    create_upright_triangle_dataset()

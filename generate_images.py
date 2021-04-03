cbc78f558d961ba9ef713718f35bad28c730ec11
Christian Garbin <garbin.christian@gmail.com>
2021-04-03 12:10:59 -0400

    Generate upright squares image dataset

--- generate_images.py
+++ generate_images.py
@@ -10,12 +10,14 @@ Sources used to write this code:
 from PIL import Image, ImageDraw
 from matplotlib import image
 import os
+import shutil
 from typing import Tuple
 
 # The directory where the generated images are stored
 IMAGE_DIRECTORY = 'images'
-# Image height/width (all images are squares)
-IMAGE_SIZE = 64
+# Canvas height/width (all images are squares)
+CANVAS_SIZE = 64
+BACKGROUND_COLOR = 255
 
 SQUARE_UPRIGHT = 'square-upright'
 SQUARE_UPRIGHT_FILE = '{}{}{}-{}'.format(IMAGE_DIRECTORY, os.path.sep, SQUARE_UPRIGHT,
@@ -23,7 +25,12 @@ SQUARE_UPRIGHT_FILE = '{}{}{}-{}'.format(IMAGE_DIRECTORY, os.path.sep, SQUARE_UP
 
 
 def _prepare():
-    """Prepare the environment to generate pictures."""
+    """Prepare the environment to generate pictures.
+
+    Ensure that we start with a clean list of images, without leftovers from previous runs (that
+    may be using differen parameters to generate images).
+    """
+    shutil.rmtree(IMAGE_DIRECTORY, ignore_errors=True)
     os.makedirs(IMAGE_DIRECTORY, exist_ok=True)
 
 
@@ -40,7 +47,7 @@ def _create_square(base_name: str, top_left: Tuple[int, ...], top_right: Tuple[i
         bottom_left (Tuple[int, ...]): [x, y] coordinate for the bottom-left corner.
         bottom_right (Tuple[int, ...]): [x, y] coordinate for the bottom-right corner.
     """
-    im = Image.new(mode='L', size=(IMAGE_SIZE, IMAGE_SIZE), color=255)
+    im = Image.new(mode='L', size=(CANVAS_SIZE, CANVAS_SIZE), color=BACKGROUND_COLOR)
     draw = ImageDraw.Draw(im)
     draw.polygon([top_left, top_right, bottom_right, bottom_left], outline=0)
     im.save(base_name.format(*top_left, *top_right, *bottom_left, *bottom_right))
@@ -54,17 +61,28 @@ def _display_grayscale_image_hex(file: str):
         print(''.join('{:02x}'.format(p) for p in data[i]))
 
 
+def create_upright_square_dataset():
+    """Create a dataset of upright square iamges."""
+    SIDE = 10
+    for y in range(2, 50, 2):
+        for x in range(2, 50, 2):
+            right_x = x+SIDE-1
+            bottom_y = y+SIDE-1
+            _create_square(SQUARE_UPRIGHT_FILE, (x, y), (right_x, y), (x, bottom_y), (right_x, bottom_y))
+
+
 def test():
     """Test code to check that grayscale images are properly generated.
 
     It creates one image, then displays the pixel values (in hexadecimal), so we can visually
     inspect that the image type and pixels values are correct.
     """
-    _prepare()
     coordinates = ((2, 2), (29, 2), (2, 29), (29, 29))
     _create_square(SQUARE_UPRIGHT_FILE, *coordinates)
     _display_grayscale_image_hex(SQUARE_UPRIGHT_FILE.format(*[c for tupl in coordinates for c in tupl]))
 
 
 if __name__ == "__main__":
-    test()
+    _prepare()
+    # test()
+    create_upright_square_dataset()
